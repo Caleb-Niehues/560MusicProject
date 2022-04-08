5b504cd02ef3977f1e6b0a5c3e24ef867ee85d1e
Jordan Austin <jordaus@ksu.edu>
2022-04-08 11:28:15 -0500

    documentation added to user model

--- MusicProject/Models/UserModel.cs
+++ MusicProject/Models/UserModel.cs
@@ -6,18 +6,39 @@ using System.Threading.Tasks;
 
 namespace MusicProject.Models
 {
+    /// <summary>
+    /// model for the user
+    /// </summary>
     public class UserModel
     {
+        /// <summary>
+        /// ID number of the user
+        /// </summary>
         private int _userID;
 
+        /// <summary>
+        /// how important the user is to the industry, bigger the number the more important they are
+        /// </summary>
         private int _userWeight;
 
+        /// <summary>
+        /// username of the user
+        /// </summary>
         private string _userName;
 
+        /// <summary>
+        /// password of the user
+        /// </summary>
         private string _password;
 
+        /// <summary>
+        /// date an time a post was added by the user
+        /// </summary>
         private DateTime _dateAdded;
 
+        /// <summary>
+        /// date and time a post was deleted by the user
+        /// </summary>
         private DateTime? _dateDeleted;
     }
 }
