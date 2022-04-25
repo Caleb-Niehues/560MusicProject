0e47245288ea0fb182987929440100b40a7353c4
Ashley Rediger <ashleyrediger@ksu.edu>
2022-04-25 12:41:00 -0500

    continued work on album repo

--- MusicProject/Sql/Procedures/MusicProject.CreateAlbum.sql
+++ MusicProject/Sql/Procedures/MusicProject.CreateAlbum.sql
@@ -1,1 +1,38 @@
-﻿CREATE OR ALTER PROCEDURE MusicProject.CreateAlbum
+﻿--Does not work
+
+CREATE OR ALTER PROCEDURE MusicProject.CreateAlbum
+	@AlbumTitle NVARCHAR(32),
+	@ReleaseDate DATE,
+	@ArtistName NVARCHAR(32),
+	@RecordLabelName NVARCHAR(32),
+	@CertificationName NVARCHAR(32)
+    --@ProducerName NVARCHAR(32)
+AS
+
+WITH CTE(AlbumTitle, ReleaseDate, ArtistID, RecordLabelID, CertificationID) AS
+	(
+		SELECT C.AlbumTitle, C.ReleaseDate, Ar.ArtistID, R.RecordLabelID, Ce.CertificationID
+		FROM
+			(
+				VALUES 
+					(@AlbumTitle, @ReleaseDate, @ArtistName, @RecordLabelName, @CertificationName)
+			) C(AlbumTitle, ReleaseDate, ArtistName, RecordLabelName, CertificationName)
+			INNER JOIN MusicProject.Artist Ar ON Ar.ArtistName = C.ArtistName
+			INNER JOIN MusicProject.RecordLabel R ON R.RecordLabelName = C.RecordLabelName
+			INNER JOIN MusicProject.Certification Ce ON Ce.CertificationName = C.CertificationName
+	)
+MERGE MusicProject.Album A
+USING CTE ON CTE.AlbumTitle = A.AlbumTitle
+	AND CTE.ArtistID = A.ArtistID
+WHEN NOT MATCHED THEN
+	INSERT(AlbumTitle, ReleaseDate, ArtistID, RecordLabelID, CertificationID)
+	VALUES(CTE.AlbumTitle, CTE.ReleaseDate, CTE.ArtistID, CTE.RecordLabelID, CTE.CertificationID);
+GO
+--handle produceralbum relation
+
+EXEC MusicProject.CreateAlbum @AlbumTitle = 'The Melodic Blue', @ReleaseDate = '2021-10-08',
+	@ArtistName = 'Kanye West', @RecordLabelName = 'Eu Limited', @CertificationName = NULL
+
+SELECT *
+FROM MusicProject.Album A
+WHERE A.AlbumTitle = 'The Melodic Blue';
