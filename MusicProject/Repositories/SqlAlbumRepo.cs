ff50b2ebc0ef1102b9b9dd860f3747483714adb6
Ashley Rediger <ashleyrediger@ksu.edu>
2022-04-25 11:48:41 -0500

    working on sqlalbumrepo

--- MusicProject/Repositories/SqlAlbumRepo.cs
+++ MusicProject/Repositories/SqlAlbumRepo.cs
@@ -29,7 +29,61 @@ namespace MusicProject.Repositories
 
         public AlbumModel FetchAlbum(string name)
         {
-            throw new NotImplementedException();
+            // Save to database.
+            using (var transaction = new TransactionScope())
+            {
+                using (var connection = new SqlConnection(connectionString))
+                {
+                    using (var command = new SqlCommand("MusicProject.FetchAlbum", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+
+                        command.Parameters.AddWithValue("Name", name);
+
+                        //var p = command.Parameters.Add("PersonId", SqlDbType.Int);
+                        //p.Direction = ParameterDirection.Output;
+
+                        connection.Open();
+
+                        using (var reader = command.ExecuteReader())
+                            return TranslateFetchAlbum(reader);
+                    }
+                }
+            }
+        }
+
+        private AlbumModel TranslateFetchAlbum(SqlDataReader reader)
+        {
+            var albumTitleOrdinal = reader.GetOrdinal("AlbumTitle");
+            var releaseDateOrdinal = reader.GetOrdinal("ReleaseDate");
+            var artistNameOrdinal = reader.GetOrdinal("ArtistName");
+            var songNameOrdinal = reader.GetOrdinal("SongName");
+            var songLengthOrdinal = reader.GetOrdinal("Length");
+            var producerNameOrdinal = reader.GetOrdinal("ProducerName");
+            var recordLabelNameOrdinal = reader.GetOrdinal("RecordLabelName");
+            var certificationOrdinal = reader.GetOrdinal("CertificationName");
+            var genreNameOrdinal = reader.GetOrdinal("GenreName");
+            var labelDateFoundedOrdinal = reader.GetOrdinal("DateFounded");
+            var labelDateClosedOrdinal = reader.GetOrdinal("DateClosed"); 
+            var labelLocationOrdinal = reader.GetOrdinal("RecordLabelLocation"); 
+
+            List<SongModel> songs = new List<SongModel>();
+            TimeSpan length = new TimeSpan();
+            List<ProducerModel> producers = new List<ProducerModel>();
+            List<RecordLabelModel> recordLabels = new List<RecordLabelModel>();
+            while (reader.Read())
+            {
+                songs.Add(new SongModel(reader.GetString(songNameOrdinal), reader.GetString(albumTitleOrdinal),
+                    reader.GetString(artistNameOrdinal), (Genre)Enum.Parse(typeof(Genre), reader.GetString(genreNameOrdinal)),
+                    reader.GetTimeSpan(songLengthOrdinal)));
+                length += reader.GetTimeSpan(songLengthOrdinal);
+                producers.Add(new ProducerModel(reader.GetString(producerNameOrdinal)));
+                recordLabels.Add(new RecordLabelModel(reader.GetString(recordLabelNameOrdinal), reader.GetDateTime(labelDateFoundedOrdinal),
+                    reader.GetDateTime(labelDateClosedOrdinal), reader.GetString(labelLocationOrdinal)));
+            }
+
+            return new AlbumModel(reader.GetString(albumTitleOrdinal), reader.GetDateTime(releaseDateOrdinal), new ArtistModel(reader.GetString(artistNameOrdinal), null),
+                songs, length, producers, recordLabels, (Certification)Enum.Parse(typeof(Certification), reader.GetString(certificationOrdinal)));
         }
 
         public IReadOnlyList<AlbumModel> GetBestPerforming(DateTime startYear, DateTime endYear, int number)
---
+++ MusicProject/Sql/Procedures/MusicProject.FetchAlbum.sql
@@ -0,0 +1,18 @@
+﻿CREATE OR ALTER PROCEDURE MusicProject.FetchAlbum
+   @Name NVARCHAR(32)
+AS
+
+SELECT A.AlbumTitle, A.ReleaseDate, Ar.ArtistName, S.SongName,
+	S.[Length], P.ProducerName, R.RecordLabelName, C.CertificationName, 
+	G.GenreName, R.DateFounded, R.DateClosed, R.RecordLabelLocation
+FROM MusicProject.Album A
+INNER JOIN MusicProject.Artist Ar ON Ar.ArtistID = A.ArtistID
+INNER JOIN MusicProject.Song S ON S.AlbumID = A.AlbumID
+INNER JOIN MusicProject.ProducerAlbum Pa ON Pa.AlbumID = A.AlbumID
+INNER JOIN MusicProject.Producer P ON P.ProducerID = Pa.ProducerID
+INNER JOIN MusicProject.RecordLabel R ON R.RecordLabelID = A.RecordLabelID
+INNER JOIN MusicProject.Certification C ON C.CertificationID = A.CertificationID
+INNER JOIN MusicProject.Genre G ON G.GenreID = S.GenreID
+WHERE A.AlbumTitle = @Name;
+
+GO
