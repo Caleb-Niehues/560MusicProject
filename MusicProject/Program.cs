79bd4f97599ed59f5479d166993f3a432d19c6b7
Caleb Niehues <chn@ksu.edu>
2022-04-25 14:05:40 -0500

    code cleanup

--- MusicProject/Program.cs
+++ MusicProject/Program.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Configuration;
 using System.Windows.Forms;
 using MusicProject.Models;
-using MusicProject.Repositories;
 using MusicProject.Views;
-using MusicProject.Mic;
 using MusicProject.Controllers;
 
 namespace MusicProject
@@ -24,12 +21,8 @@ namespace MusicProject
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             
-            string testConnect = ConfigurationManager.ConnectionStrings["configConnectionCaleb"].ConnectionString;//just one call in maincontroller and pass throughout in actual practice
-            MainController controller = new MainController(testConnect);
+            var controller = new MainController();
             LogInView.InitializeDelegates(controller.CredentialCheck, controller.CreateUser, controller.DeleteUser);
-            //var testRepo = new SqlUserRepo(testConnect);
-            //UserModel testUser;
-            //if (testRepo.FetchUser("3ero8x", "meestamastae3", out testUser));
           
             Application.Run(new MainView(controller));//confirmed issue of needing to run - does this mean the powershell stuff is required?
         }
--- MusicProject/Views/MainView.cs
+++ MusicProject/Views/MainView.cs
@@ -8,7 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using MusicProject.Controllers;
 using MusicProject.Models;
-using MusicProject.Mic;
 using System.Windows.Forms;
 
 namespace MusicProject.Views
@@ -16,9 +15,8 @@ namespace MusicProject.Views
     public delegate void UpdateUser(UserModel user);
     public partial class MainView : Form
     {
-        #region Test Album/Load Order
+        private UserModel activeUser;
         private ReviewModel reviewModel = null;
-        private UserModel user;
 
         private List<PersonModel> members = new List<PersonModel>();
         private ArtistModel artistModel;
@@ -29,83 +27,34 @@ namespace MusicProject.Views
 
         private List<RecordLabelModel> recordLabels = new List<RecordLabelModel>();
 
-        private AlbumModel test;
-
-        public void TestInitialization()
-        {
-            members.Add(new PersonModel("Kendrick Lamar Duckworth", new DateTime(1987, 6, 17), null, "Compton, CA, USA"));
-            artistModel = new ArtistModel("Kendrick Lamar", members);//could possibly use solo query to match name
-
-            #region Adding Songs
-            //songs.Add(new SongModel("Wesley's Theory", test, Genre.HipHop, new TimeSpan(0, 4, 47)));
-            //songs.Add(new SongModel("For Free? (Interlude)", test, Genre.HipHop, new TimeSpan(0, 2, 10)));
-            //songs.Add(new SongModel("King Kunta", test, Genre.HipHop, new TimeSpan(0, 3, 54)));
-            //songs.Add(new SongModel("Institutionalized", test, Genre.HipHop, new TimeSpan(0, 4, 31)));
-            //songs.Add(new SongModel("These Walls", test, Genre.HipHop, new TimeSpan(0, 5, 0)));
-            //songs.Add(new SongModel("U", test, Genre.HipHop, new TimeSpan(0, 4, 28)));
-            //songs.Add(new SongModel("Alright", test, Genre.HipHop, new TimeSpan(0, 3, 39)));
-            //songs.Add(new SongModel("For Sale? (Interlude)", test, Genre.HipHop, new TimeSpan(0, 4, 51)));
-            //songs.Add(new SongModel("Momma", test, Genre.HipHop, new TimeSpan(0, 4, 43)));
-            //songs.Add(new SongModel("Hood Politics", test, Genre.HipHop, new TimeSpan(0, 4, 52)));
-            //songs.Add(new SongModel("How Much a Dollar Cost", test, Genre.HipHop, new TimeSpan(0, 4, 21)));
-            //songs.Add(new SongModel("Complexion (A Zulu Love)", test, Genre.HipHop, new TimeSpan(0, 4, 23)));
-            //songs.Add(new SongModel("The Blacker the Berry", test, Genre.HipHop, new TimeSpan(0, 5, 28)));
-            //songs.Add(new SongModel("You Ain't Gotta Lie", test, Genre.HipHop, new TimeSpan(0, 4, 1)));
-            //songs.Add(new SongModel("I", test, Genre.HipHop, new TimeSpan(0, 5, 36)));
-            //songs.Add(new SongModel("Mortal Man", test, Genre.HipHop, new TimeSpan(0, 12, 7)));
-            #endregion
-
-            recordLabels.Add(new RecordLabelModel("TDE", new DateTime(2004, 1, 1), null, "Carson, CA, USA"));
-            recordLabels.Add(new RecordLabelModel("Aftermath Entertainment", new DateTime(1996, 3, 22), null, "Santa Monica, CA, USA"));
-            recordLabels.Add(new RecordLabelModel("Interscope Records", new DateTime(1990, 1, 1), null, "Santa Monica, CA, USA"));
-
-            #region Adding Producers
-            producerModels.Add(new ProducerModel("Boi-1da"));
-            producerModels.Add(new ProducerModel("Flippa"));
-            producerModels.Add(new ProducerModel("Flying Lotus"));
-            producerModels.Add(new ProducerModel("Knxledge"));
-            producerModels.Add(new ProducerModel("KOZ"));
-            producerModels.Add(new ProducerModel("Larrance"));
-            producerModels.Add(new ProducerModel("Dopson"));
-            producerModels.Add(new ProducerModel("LoveDragon"));
-            producerModels.Add(new ProducerModel("Pharrel Williams"));
-            producerModels.Add(new ProducerModel("Rahki"));
-            producerModels.Add(new ProducerModel("Sounwave"));
-            producerModels.Add(new ProducerModel("Tae Beast"));
-            producerModels.Add(new ProducerModel("Taz Arnold"));
-            producerModels.Add(new ProducerModel("Terrace Martin"));
-            producerModels.Add(new ProducerModel("Thundercat"));
-            producerModels.Add(new ProducerModel("Tommy Black"));
-            producerModels.Add(new ProducerModel("Whoarei"));
-            #endregion
-
-            test = new AlbumModel("To Pimp A Butterfly", new DateTime(2015, 3, 15), artistModel, songs, new TimeSpan(0, 78, 51), producerModels, recordLabels, Certification.Platinum);
-        }
-        #endregion
+        private AlbumModel selectedAlbum;
 
         private void RegisterLogin(UserModel user)
         {
-            if (user == null) uxUserLabel.Text = "Not logged in";
-            else uxUserLabel.Text = "Logged in as: " + user.Name;
+            activeUser = user;
+            if (activeUser == null)
+                uxUserLabel.Text = "Not logged in";
+            else
+                uxUserLabel.Text = "Logged in as: " + activeUser.Name;
         }
 
         public LogInView login;
-        public DatabaseProxy proxy;
         public CheckCredentials check;
         public MainView(MainController controller)
         {
             InitializeComponent();
-            user = controller.ActiveUser;
-            //TestInitialization();
+            activeUser = controller.ActiveUser;
+            login = new LogInView(RegisterLogin);
+            login.Show();
         }
 
         private void uxLookUpText_KeyDown(object sender, KeyEventArgs e)
         {
             //check if enter key pressed and run search
-            uxSongList.DataSource = test.Songs;
-            uxProducerList.DataSource = test.Producers;
-            uxPersonList.DataSource = members;
-            uxRecordLabelList.DataSource = recordLabels;
+            //uxSongList.DataSource = test.Songs;
+            //uxProducerList.DataSource = test.Producers;
+            //uxPersonList.DataSource = members;
+            //uxRecordLabelList.DataSource = recordLabels;
         }
 
         private void uxLogIn_Click(object sender, EventArgs e)
